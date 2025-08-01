7037c754d36672f6b2208126fc7bed9ad8c4ec2c
leopilots <leonardoyalejandrosr@gmail.com>
2025-07-31 20:11:09 -0400

    fix: search/filter products

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// button bar
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-main").classList.toggle("show");
 });
--- js/products.js
+++ js/products.js
@@ -1,21 +1,3 @@
-// zoom de imágenes
-const zoomableImages = document.querySelectorAll('.zoomable');
-const overlay = document.getElementById('overlay');
-const overlayImg = document.getElementById('overlay-img');
-
-zoomableImages.forEach(img => {
-  img.addEventListener('click', () => {
-    overlayImg.src = img.src;
-    overlay.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
-  });
-});
-
-overlay.addEventListener('click', () => {
-  overlay.style.display = 'none';
-  document.body.style.overflow = '';
-});
-
 // barra de búsqueda y filtro de productos
 const searchInput = document.querySelector('.barra-busqueda input');
 const searchButton = document.querySelector('.barra-busqueda button');
